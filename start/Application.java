b7c4262eb1c6cdefebd43b343173872dfdbcfa34
Jason Taylor <jason@jasongtaylor.com>
2016-04-20 02:05:32 -0400

    Create Application.java

---
+++ start/Application.java
@@ -0,0 +1,14 @@
+package clinic.programming;
+
+public class Application {
+    
+    public Application() {
+        System.out.println ("Inside Application");
+    }
+	
+	  // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+		  System.out.println ("Starting Application");
+		  Application app = new Application();
+    }
+}
