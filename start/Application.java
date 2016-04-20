705eabefbc7c5ac11c60c73ee8d66f79da438ed2
Jason Taylor <jason@jasongtaylor.com>
2016-04-20 02:07:14 -0400

    Update Application.java

--- start/Application.java
+++ start/Application.java
@@ -1,14 +1,14 @@
-package clinic.programming;
+package clinic.programming.training;
 
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
-	
-	  // method main(): ALWAYS the APPLICATION entry point
+
+    // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-		  System.out.println ("Starting Application");
-		  Application app = new Application();
+    	System.out.println ("Starting Application");
+	Application app = new Application();
     }
 }
