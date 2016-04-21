eafcdd9fa957194db5d633f2ab1fc39781f499f8
Jason Taylor <jason@jasongtaylor.com>
2016-04-20 23:26:16 -0400

    Create ApplicationTest.java

---
+++ test/ApplicationTest.java
@@ -0,0 +1,47 @@
+package training.maven.quick;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+    private Application app;
+    
+    @Before
+    public void setup() {
+        app = new Application();
+    }
+    
+    
+    @Test
+    public void testWordCountString() {
+        int count = app.countWords("this is a test");
+        assertTrue(count == 4);
+    }
+
+    @Test
+    public void testWordCountEmpty() {
+        int count = app.countWords("");
+        assertTrue(count == 0);
+    }
+
+    @Test
+    public void testWordCountNull() {
+        int count = app.countWords(null);
+        assertTrue(count == 0);
+    }
+
+    @Test
+    @Ignore
+    public void testWordCountMore() {
+        int count = app.countWords("this will be skipped");
+        assertTrue(count == 3);
+    }
+
+    @Test
+    public void testWordCountSingle() {
+        int count = app.countWords("test");
+        assertTrue(count == 1);
+    }
+}
